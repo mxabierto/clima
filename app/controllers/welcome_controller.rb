c0475bafe5695ee7948f5bb43ec1bc13bbcaa815
Matias <matiasrenta@gmail.com>
2015-11-05 13:15:02 -0600

    arreglos en layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,22 @@
+require 'csv'
+
 class WelcomeController < ApplicationController
   def index
     set_content_title('fa fa-lg fa-fw fa-cube', ['Welcome!'])
+    csv = CSV.new(File.new('conjunto_landing_3_rows.csv'), :headers => true, :header_converters => :symbol)
+    csv.each do |row|
+      puts "mmmmmmmmmm: #{row[0]}"
+    end
+  end
+
+  def page1
+    # obtener json con los siguientes datos por cada una de las variables:
+    # 1. la posición de mexico en el ranking
+    # 2. % de mexico sobre el total
+    csv = CSV.new(File.new('conjunto_landing_3_rows.csv'), :headers => true, :header_converters => :symbol)
+    csv.to_a.each do |row|
+      puts "mmmmmmmmmm: #{row.class.name}"
+    end
+    # obtener json para la gráfica (el emisiones_v2.csv)
   end
 end
